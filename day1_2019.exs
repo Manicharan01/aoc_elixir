fa37926bfa130c5a86c6e8e3b6019b723e0c2310
Manicharan <manicharan150@gmail.com>
2024-10-30 16:52:28 +0530

    AOC 2019 Day 1 in elixir language

---
+++ day1_2019.exs
@@ -0,0 +1,19 @@
+defmodule Recursion do
+  def calculate_fuel_for_fuel(n) when n > 0 do
+    n + calculate_fuel_for_fuel(div(n, 3) - 2)
+  end
+
+  def calculate_fuel_for_fuel(n) when n <= 0 do
+    0
+  end
+end
+
+File.stream!("C:/Users/Manicharan Kollipara/Desktop/aoc/day1.txt", :line)
+|> Enum.to_list()
+|> Enum.map(fn x -> String.trim(x) end)
+|> Enum.filter(fn x -> String.trim(x) != "" end)
+|> Enum.map(fn x -> String.to_integer(x) end)
+|> Enum.map(fn x -> div(x, 3) - 2 end)
+|> Enum.map(fn x -> Recursion.calculate_fuel_for_fuel(x) end)
+|> Enum.sum()
+|> IO.puts()
